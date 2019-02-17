ad9ca13fc35440f6114a8d1adb165321a4c26630
Mengqing Jiang <mengqing@studebaker.ahs.ri.cmu.edu>
2019-02-16 22:05:35 -0500

    Update parameters

--- process_houses.sh
+++ process_houses.sh
@@ -2,24 +2,24 @@
 
 toolboxdir="//home/mengqing/SUNCGtoolbox"
 catfile="/data/SUNCG/metadata/ModelCategoryMapping.csv"
-outputdir="."
+outputdir="new_sample_all_camera_2"
 
 echo "Processing $1"
 cd $1
-# if [ -d "$outputdir" ]; then
-#     rm -rf $outputdir
-# fi
-# mkdir -m 775 $outputdir
-# chgrp users $outputdir
+if [ -d "$outputdir" ]; then
+    rm -rf $outputdir
+fi
+mkdir -m 775 $outputdir
+chgrp users $outputdir
 
 # $toolboxdir/gaps/bin/x86_64/scn2scn house.json house.obj
 
 # generate cameras
 $toolboxdir/gaps/bin/x86_64/scn2cam house.json $outputdir/cameras.txt -categories $catfile \
     -width 320 -height 240 -xfov 0.54 \
-    -position_sampling 1.5 -eye_height 1.5 -eye_height_radius 0.1 \
+    -position_sampling 1.5 -eye_height 1.38 -eye_height_radius 0.1 \
     -min_visible_objects 0 \
-    -angle_sampling 0.628 \
+    -angle_sampling 0.628 -eye_pitch_angle 0.2\
     -output_camera_names $outputdir/camera_names.txt \
     -output_camera_extrinsics $outputdir/extrinsics.txt \
     -output_camera_intrinsics $outputdir/intrinsics.txt \
@@ -31,7 +31,7 @@ mkdir -p $outputdir/imgs
 #$toolboxdir/gaps/bin/x86_64/scn2img house.json $outputdir/cameras.txt $outputdir/imgs \
 #    -width 320 -height 240 -capture_color_images -capture_depth_images -capture_node_images -v
 $toolboxdir/gaps/bin/x86_64/scn2img house.json $outputdir/cameras.txt $outputdir/imgs \
-    -kinect_noise_fraction 0.01 \
+    -kinect_noise_fraction 0.01 -kinect_min_reflection 0\
     -width 320 -height 240 -capture_color_images -capture_kinect_images -capture_node_images -v
 
 # generate partial point cloud
